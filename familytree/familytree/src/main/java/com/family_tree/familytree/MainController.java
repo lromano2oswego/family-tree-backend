dbbac2803bc6283c60381cc0a4130978f4db6861
lromano2oswego <157433850+lromano2oswego@users.noreply.github.com>
2024-10-25 11:42:25 -0400

    Update MainController.java

--- familytree/familytree/src/main/java/com/family_tree/familytree/MainController.java
+++ familytree/familytree/src/main/java/com/family_tree/familytree/MainController.java
@@ -17,15 +17,15 @@ public class MainController {
     private UserRepository userRepository;
 
     @PostMapping(path="/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String name,
-                                            @RequestParam String email) {
+    public @ResponseBody String addNewUser (@RequestParam String username,
+                                            @RequestParam String emailAddress) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
-        User n = new User();
-        n.setName(name);
-        n.setEmail(email);
-        userRepository.save(n);
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(emailAddress);
+        userRepository.save(user);
         return "Saved";
     }
 
