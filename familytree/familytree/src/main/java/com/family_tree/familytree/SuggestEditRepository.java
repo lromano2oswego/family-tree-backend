805dd4ffd0e53afd201042561a0ffbf3b8fa08e3
lromano2oswego <157433850+lromano2oswego@users.noreply.github.com>
2024-11-21 16:44:33 -0500

    Update SuggestEditRepository.java

--- familytree/familytree/src/main/java/com/family_tree/familytree/SuggestEditRepository.java
+++ familytree/familytree/src/main/java/com/family_tree/familytree/SuggestEditRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface SuggestEditRepository extends JpaRepository<SuggestEdit, Integer> {
     // Find suggested edits for a specific family member by their ID
-    List<SuggestEdit> findByFamilyMember_Id(Integer memberId);
+    List<SuggestEdit> findByMember_MemberId(Integer memberId);
 
     // Find suggested edits by the ID of the user who suggested them
     List<SuggestEdit> findBySuggestedBy_Id(Integer userId);
