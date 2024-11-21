0f89e654da3c871f622fa7dce09ad0a030e8450e
lromano2oswego <157433850+lromano2oswego@users.noreply.github.com>
2024-11-21 17:28:26 -0500

    Update to fix SuggestEditRepository.java

--- familytree/familytree/src/main/java/com/family_tree/familytree/SuggestEditRepository.java
+++ familytree/familytree/src/main/java/com/family_tree/familytree/SuggestEditRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SuggestEditRepository extends JpaRepository<SuggestEdit, Integer> {
-    // Find suggested edits for a specific family member by their ID
-    List<SuggestEdit> findByMember_MemberId(Integer memberId);
-
     // Find suggested edits by the ID of the user who suggested them
     List<SuggestEdit> findBySuggestedBy_Id(Integer userId);
 
@@ -17,11 +14,11 @@ public interface SuggestEditRepository extends JpaRepository<SuggestEdit, Intege
     List<SuggestEdit> findBySuggestionStatus(SuggestionStatus suggestionStatus);
 
 
-    List<SuggestEdit> findByMember_MemberId(Integer memberId);
+    List<SuggestEdit> findByFamilyMember_MemberId(Integer memberId);
 
-    List<SuggestEdit> findByTreeId(Integer treeId);
+    List<SuggestEdit> findByFamilyMember_FamilyTree_Id(Integer treeId);
 
     void deleteBySuggestedById(Integer userId);
 
-    void deleteByMemberId(Integer memberId);
+    void deleteByFamilyMember_MemberId(Integer memberId);
 }
